ef7cdcfedd251f4af30859b8c5e57f190e2b5b61
Luka Bracanovic <lbracano@gmail.com>
2023-10-24 22:23:54 +0200

    Fix createSchemaFieldRule validation

--- src/utils/createSchemaFieldRule.spec.ts
+++ src/utils/createSchemaFieldRule.spec.ts
@@ -1,12 +1,15 @@
 import createSchemaFieldRule from "./createSchemaFieldRule";
+import { FormInstance } from "antd";
+import { RuleObject } from "rc-field-form/lib/interface";
+
 import { NameSchema, NestedRefinedSchema } from "../fixtures";
-import { FormInstance } from "../types";
 
-const mockFormInstance = (values: {}): FormInstance => ({
-  getFieldsValue: () => values,
-});
+const mockFormInstance = (values: {}) =>
+  ({
+    getFieldsValue: () => values,
+  }) as FormInstance;
 
-const mockFormFieldRule = (field: string) => ({ field });
+const mockFormFieldRule = (field: string) => ({ field }) as RuleObject;
 
 describe("createSchemaFieldValidator", () => {
   it("should validate successfully NameSchema values", async () => {
@@ -14,44 +17,44 @@ describe("createSchemaFieldValidator", () => {
     const formInstance = mockFormInstance({ name: "Test" });
     const fieldRule = mockFormFieldRule("name");
 
-    await expect(rule(formInstance).validator(fieldRule)).resolves.toEqual(
-      undefined,
-    );
+    await expect(
+      rule(formInstance).validator?.(fieldRule, {}, () => {}),
+    ).resolves.toEqual(undefined);
   });
   it("should reject invalid NameSchema values", async () => {
     const rule = createSchemaFieldRule(NameSchema);
     const formInstance = mockFormInstance({});
     const fieldRule = mockFormFieldRule("name");
 
-    await expect(rule(formInstance).validator(fieldRule)).rejects.toEqual(
-      "Required",
-    );
+    await expect(
+      rule(formInstance).validator?.(fieldRule, {}, () => {}),
+    ).rejects.toEqual("Required");
   });
   it("should validate successfully NestedRefinedSchema values", async () => {
     const rule = createSchemaFieldRule(NestedRefinedSchema);
     const formInstance = mockFormInstance({ user: { name: "Luka" } });
     const fieldRule = mockFormFieldRule("name");
 
-    await expect(rule(formInstance).validator(fieldRule)).resolves.toEqual(
-      undefined,
-    );
+    await expect(
+      rule(formInstance).validator?.(fieldRule, {}, () => {}),
+    ).resolves.toEqual(undefined);
   });
   it("should reject undefined NestedRefinedSchema values", async () => {
     const rule = createSchemaFieldRule(NestedRefinedSchema);
     const formInstance = mockFormInstance({});
     const fieldRule = mockFormFieldRule("user.name");
 
-    await expect(rule(formInstance).validator(fieldRule)).rejects.toEqual(
-      "Required",
-    );
+    await expect(
+      rule(formInstance).validator?.(fieldRule, {}, () => {}),
+    ).rejects.toEqual("Required");
   });
   it("should reject invalid NestedRefinedSchema values", async () => {
     const rule = createSchemaFieldRule(NestedRefinedSchema);
     const formInstance = mockFormInstance({ user: { name: "test" } });
     const fieldRule = mockFormFieldRule("user.name");
 
-    await expect(rule(formInstance).validator(fieldRule)).rejects.toEqual(
-      "Must be Luka",
-    );
+    await expect(
+      rule(formInstance).validator?.(fieldRule, {}, () => {}),
+    ).rejects.toEqual("Must be Luka");
   });
 });
