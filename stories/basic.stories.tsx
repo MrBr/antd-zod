360f0e82a019e99a626b4aedaf4eaa5b6711490d
Luka Bracanovic <lbracano@gmail.com>
2024-01-18 07:36:05 +0100

    Add nested object story example

--- stories/basic.stories.tsx
+++ stories/basic.stories.tsx
@@ -6,12 +6,17 @@ import { createSchemaFieldRule } from "../src";
 const VALUES = ["Male", "Female"] as const;
 const Genders = z.enum(VALUES);
 
+const Cities = z.enum(["New York", "Peking", "Paris", "London"]);
+
 const BasicSchema = z.object({
   name: z.string().refine((value) => value.length > 2, {
     message: "Must have more than 2 chars",
   }),
   height: z.number(),
   gender: Genders.optional(),
+  address: z.object({
+    city: Cities,
+  }),
 });
 
 const rule = createSchemaFieldRule(BasicSchema);
@@ -25,12 +30,23 @@ const BasicForm = () => {
       <Form.Item label="Enter height" name="height" rules={[rule]}>
         <InputNumber />
       </Form.Item>
-      <Form.Item label="Enter gender" name="gender" rules={[rule]}>
+      <Form.Item label="Select gender" name="gender" rules={[rule]}>
         <Select
           options={[
             { value: "Male" },
             { value: "Female" },
             { value: "Invalid option" },
+          ]}
+        />
+      </Form.Item>
+      <Form.Item label="Select city" name={["address", "city"]} rules={[rule]}>
+        <Select
+          options={[
+            { value: "New York" },
+            { value: "Peking" },
+            { value: "Paris" },
+            { value: "London" },
+            { value: "Invalid city" },
           ]}
         />
       </Form.Item>
