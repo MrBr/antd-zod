d3a36aa9f6798702c5b14cbc828cafd030149248
杨伟杰 <justlikesuolong@outlook.com>
2024-03-11 03:55:31 +0000

    Compatible with antd component

--- src/utils/createSchemaFieldRule.ts
+++ src/utils/createSchemaFieldRule.ts
@@ -4,27 +4,37 @@ import { AntdFormZodSchema } from "../types";
 import validateFields from "./validateFields";
 import prepareValues from "./prepareValues";
 
+interface CreateSchemaFieldRuleOptions {
+  ignorePrefix?: boolean;
+}
+
 const createSchemaFieldRule =
-  <T extends ZodRawShape>(schema: AntdFormZodSchema<T>): RuleRender =>
-  ({ getFieldsValue }) => ({
-    validator: (rule) =>
-      new Promise(async (resolve, reject) => {
-        // AsyncValidator adds field - rc-field-form type isn't correct
-        // https://github.com/search?q=repo%3Ayiminghe%2Fasync-validator%20fullField&type=code
-        const { field } = rule as { field: string };
-        const values = getFieldsValue();
-        const errors = await validateFields<T>(
-          schema,
-          prepareValues(schema, values),
-        );
+  <T extends ZodRawShape>(schema: AntdFormZodSchema<T>, options: CreateSchemaFieldRuleOptions = {}): RuleRender =>
+    ({ getFieldsValue }) => ({
+      validator: (rule) =>
+        new Promise(async (resolve, reject) => {
+          // AsyncValidator adds field - rc-field-form type isn't correct
+          // https://github.com/search?q=repo%3Ayiminghe%2Fasync-validator%20fullField&type=code
+          let { field } = rule as { field: string };
+          if (options.ignorePrefix) {
+            const indexOf = field.indexOf(".");
+            if (indexOf !== -1) {
+              field = field.slice(indexOf + 1);
+            }
+          }
+          const values = getFieldsValue();
+          const errors = await validateFields<T>(
+            schema,
+            prepareValues(schema, values),
+          );
 
-        if (!!errors && errors[field]) {
-          reject(errors[field]);
-          return;
-        }
+          if (!!errors && errors[field]) {
+            reject(errors[field]);
+            return;
+          }
 
-        resolve(undefined);
-      }),
-  });
+          resolve(undefined);
+        }),
+    });
 
 export default createSchemaFieldRule;
