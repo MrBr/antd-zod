586538edb89cb2f2425bc3773176fc247bf9d0c9
Luka Bracanovic <lbracano@gmail.com>
2025-04-10 20:40:58 +0200

    refactor: multiple errors schema

--- src/utils/formatErrors.spec.ts
+++ src/utils/formatErrors.spec.ts
@@ -2,13 +2,6 @@ import z, { ZodError, ZodIssue } from "zod";
 import formatErrors from "./formatErrors";
 import prepareValues from "./prepareValues";
 
-const fakeIssues: ZodIssue[] = [
-  { code: "custom", message: "Error one", path: ["field"] },
-  { code: "custom", message: "Error two", path: ["field"] },
-];
-
-const fakeZodError = new ZodError(fakeIssues);
-
 const schema = z.object({ field: z.string() });
 
 describe("formatErrors", () => {
@@ -53,9 +46,24 @@ describe("formatErrors", () => {
 
     expect(formattedErrors).toEqual({ "prop.child": ["Required"] });
   });
-  it("should return multiple errors", () => {
-    const formattedErrors = formatErrors(schema, fakeZodError);
-    console.log(formattedErrors)
-    expect(formattedErrors).toEqual({ field: ["Error one", "Error two"] });
+  it("should return multiple errors", async () => {
+    const multipleErrorSchema = z.object({
+      email: z.string().email().min(5).max(15).includes(".com"),
+    });
+
+    const res = await multipleErrorSchema.safeParseAsync(
+      prepareValues(multipleErrorSchema, { email: "t" }),
+    );
+    if (res.success) {
+      return;
+    }
+    const formattedErrors = formatErrors<{}>(schema, res.error);
+    expect(formattedErrors).toEqual({
+      email: [
+        "Invalid email",
+        "String must contain at least 5 character(s)",
+        'Invalid input: must include ".com"',
+      ],
+    });
   });
 });
